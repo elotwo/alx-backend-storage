1805da935b26c70144f046c088f0ca2f462c9fbf
elotwo <obiagwu94@gmail.com>
2024-10-17 16:23:28 +0100

    advanced Sql

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,11 @@
--- This create a table with a unique email
+-- This script create a table with a unique email
+-- and adds a country column with specified enumerated values.
 
 -- Create the table if it does not exist
 CREATE TABLE IF NOT EXISTS users(
 id INT NOT NULL AUTO_INCREMENT,
 email VARCHAR (225) NOT NULL UNIQUE,
 name VARCHAR (255) NOT NULL,
+country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
 PRIMARY KEY (id)
 );
