d955d22696152721615e5b000b318b8ab33c125a
elotwo <obiagwu94@gmail.com>
2024-10-17 17:20:41 +0100

    me

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,1 +1,19 @@
-SELECT * FROM 
+CREATE TEMPORARY TABLE ranked_countries AS
+SELECT 
+    origin,
+    SUM(fans) AS nb_fans
+FROM 
+    metal_bands
+GROUP BY 
+origin
+ORDER BY 
+    nb_fans DESC;
+SELECT 
+    origin,
+    nb_fans,
+    @rank := @rank + 1 AS rank
+    (SELECT @rank := 0) r 
+FROM 
+    ranked_countries;
+ORDER BY 
+    nb_fans DESC;
