f4a241764e5b9ad8368758d0447066b04f98f30f
elotwo <obiagwu94@gmail.com>
2024-10-25 22:41:02 +0100

    update

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,2 +1,10 @@
-CREATE TEMPORARY TABLE rocks;
-SELECT FROM  metal_bands WHERE band_name = Glam rock
+SELECT
+   band_name AS band_name,
+   COALESCE(split, 2022) - formed AS lifespan
+FROM 
+metal_bands
+WHERE 
+style = 'Glam rock'
+AND formed <= 2022
+ORDER BY 
+lifespan DESC;
